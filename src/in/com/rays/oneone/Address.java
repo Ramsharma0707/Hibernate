e528e35d6d9446daa7c9c71b3b24f6339c0b8c8d
Ramsharma1994 <ramsharmaa51@gmail.com>
2024-02-21 15:35:18 +0530

    done

---
+++ src/in/com/rays/oneone/Address.java
@@ -0,0 +1,31 @@
+package in.com.rays.oneone;
+
+public class Address {
+
+	private int id;
+	 private String stree;
+	 private String city;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStree() {
+		return stree;
+	}
+	public void setStree(String stree) {
+		this.stree = stree;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	 
+	 
+	 
+	 
+	
+}
