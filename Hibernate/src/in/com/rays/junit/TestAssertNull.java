80b2530f11f8eac983580d280aae436a955e5dab
Ramsharma1994 <ramsharmaa51@gmail.com>
2024-03-01 11:15:19 +0530

    done

---
+++ Hibernate/src/in/com/rays/junit/TestAssertNull.java
@@ -0,0 +1,21 @@
+package in.com.rays.junit;
+
+
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestAssertNull extends TestCase {
+
+	@Test
+	public void testFindByPk() throws Exception {
+
+		UserBean bean = UserModel.findByPk(11);
+
+		//assertNull("user is null", bean);
+		 assertTrue(bean == null);
+
+	}
+}
+
---
+++ Hibernate/src/in/com/rays/subclass/Cash.java
@@ -0,0 +1,7 @@
+package in.com.rays.subclass;
+
+import in.com.rays.inheritance.Payment;
+
+public class Cash extends Payment {
+
+}
